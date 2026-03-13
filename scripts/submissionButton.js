05c1e8ed5525e46b44aa3a0b79bb82a974e6ad20
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-03-13 11:46:32 -0500

    Submission button module

---
+++ scripts/submissionButton.js
@@ -0,0 +1,15 @@
+// import { placeOrder } from "./transientState.js"
+
+// click event handler
+const handlePurchaseSubmission = (clickEvent) => {
+    if (clickEvent.target.id === "submission-button") {
+        console.log("Purchase Combo button clicked")
+        placeOrder()
+    }
+}
+
+export const submissionButton = () => {
+    document.addEventListener("click", handlePurchaseSubmission)
+
+    return `<button id="submission-button">Purchase Combo</button>`
+}
