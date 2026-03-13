9b20bb4b4b3665c27b3772a0fab524fd8862ffa1
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-03-13 14:50:36 -0500

    Building Sides module

--- scripts/SideDishes.js
+++ scripts/SideDishes.js
@@ -1,4 +1,31 @@
-export const Sides = () => {
+// Import {} from transientState.js
+
+// Create change handler func
+
+export const Sides = async () => {
+    // Fetch sides from API
+    const response = await fetch("http://localhost:8088/sides")
+    const sides = await response.json()
+
+    // console.log(sides)
+
+    // Add an event listener
+
+    let optionsHTML = ""
+
+    const arrayOfOptions = sides.map(
+        (side) => {
+            return `<div>
+                <input type="radio" name="side" value="${side.id}" /> ${side.title}
+            </div>`
+        }
+    )
+
+    // console.log(arrayOfOptions)
+
+    optionsHTML += arrayOfOptions.join("")
+
+    // console.log(optionsHTML)
 
     return html
 }
