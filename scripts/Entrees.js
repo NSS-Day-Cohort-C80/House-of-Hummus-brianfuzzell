eafa0c90b21345625a6b79648b5006565c1e8b30
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-03-13 11:38:13 -0500

    Initial commit

---
+++ scripts/Entrees.js
@@ -0,0 +1,19 @@
+// Import {} from transientState.js
+
+// Create change handler func
+
+// Create & export entreesOptions func
+export const entreesOptions = async () => {
+    // Fetch entrees from API
+
+    // console.log(entrees)
+
+    // Add an event listener
+
+    let optionsHTML = ""
+
+    optionsHTML += ''
+
+
+}
+    
--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,43 @@ import { FoodTruck } from "./FoodTruck.js"
 
 const mainContainer = document.querySelector("#container")
 
-const renderAllHTML = () => {
+const renderAllHTML = async () => {
+    // Assign imported functions to HTML variables
+    // const foodTruckHTML = FoodTruck()
+
+    // Create composedHTML
+    const composedHTML = `
+        <h1>Laura Kathryn's House of Hummus</h1>
+
+        <article>
+            <section>
+                <h2>Base Dish</h2>
+
+            </section>
+
+            <section>
+                <h2>Vegetable</h2>
+                
+            </section>
+
+            <section>
+                <h2>Sides</h2>
+                
+            </section>
+        </article>
+
+        <article>
+        
+        </article>
+
+        <article>
+        
+        </article>
+    `
+    
     mainContainer.innerHTML = FoodTruck()
 }
 
-renderAllHTML()
+// Add an event listener for our new custom event
 
+renderAllHTML()
